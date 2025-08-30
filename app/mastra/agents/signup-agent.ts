04408b6f70a5d88bb6eb5d7db2322da34bf7c870
Rick Moy <rickmoy@gmail.com>
2025-08-29 17:38:47 -0700

    misc

--- app/mastra/agents/signup-agent.ts
+++ app/mastra/agents/signup-agent.ts
@@ -10,9 +10,10 @@ export const AgentState = z.object({
 	plan: z.array(z.object({ milestone: z.string(), completed: z.boolean() })).default([]),
 	signupChatData: z
 		.object({
-			problem: z.string().optional(),
-			challenges: z.string().optional(),
+			goal: z.string().optional(),
+			questions: z.string().optional(),
 			content_types: z.string().optional(),
+			challenges: z.string().optional(),
 			interview_recordings: z.string().optional(),
 			other_feedback: z.string().optional(),
 			completed: z.boolean().optional(),
@@ -26,13 +27,14 @@ export const signupAgent = new Agent({
       You are a onboarding assistant, whose goal is to collect data from a user who has just signed up for the app and save it in database AgentState signupChatData
 			in the table user_settings signup_data for the user_id EVERY TIME YOU GET NEW INFORMATION.
 
-      Be brief and to the point. Ask 4 questions
-			- the problem they are researching
-			- the challenges they're facing
+      Be brief and to the point. Ask 4 questions about their use case for conversational intelligence:
+			- the goal you're trying to achieve
+			- the questions you need to answer in order to help you achieve your goal
+			- the challenges in getting those answers
 			- the content types they want to analyze
 			- do you have interview recordings or transcripts to analyze now
 
-			- after you collected and saved the data, say "Thanks buddy" and redirect the user to the home page at {HOST}{PATHS.HOME} = https://upsight.fly.dev/home
+			- As soon as you have captured the goal, questions, challenges, content types, and interview recordings, save the data, and say "Thanks. Looking forward to heping you out here." and redirect the user to the home page at {HOST}{PATHS.HOME} = https://upsight.fly.dev/home
 `,
 	model: openai("gpt-4o-mini"),
 	tools: {
