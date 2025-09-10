ea22c177dfce02f05a7f18c513ae29c25ac799eb
jettmoy <jettmoy@gmail.com>
2025-09-10 14:03:46 -0400

    feat: make ai-sdk defualt /signup-chat

--- app/features/signup-chat/routes.ts
+++ app/features/signup-chat/routes.ts
@@ -3,8 +3,8 @@ import { index, layout, prefix, type RouteConfig, route } from "@react-router/de
 export default [
 	route("/signup-chat-old", "./features/signup-chat/pages/signup-chat.tsx"),
 	route("/signup-chat/completed", "./features/signup-chat/pages/chat-completed.tsx"),
-	route("/signup-chat/ai-sdk", "./features/signup-chat/pages/ai-sdk-chat.tsx"),
+	route("/signup-chat", "./features/signup-chat/pages/ai-sdk-chat.tsx"),
 	route("api/chat/signup", "./features/signup-chat/api/signup-agent.tsx"),
-	route("/signup-chat", "./features/signup-chat/pages/assistant-ui-chat.tsx"),
+	route("/signup-chat/assistant-ui", "./features/signup-chat/pages/assistant-ui-chat.tsx"),
 	...prefix("aichat", [layout("./features/signup-chat/layout.tsx", [index("./features/signup-chat/pages/chat.tsx")])]),
 ] satisfies RouteConfig
